f541069d8f39ec3027b6cd2eef6647aa571f164b
Fergus Roache <fergus@msupply.foundation>
2025-01-15 10:04:42 +1300

    Add test retrieving comments

---
+++ .github/workflows/query_info.js
@@ -0,0 +1,22 @@
+const { Octokit } = require('@octokit/core');
+
+const octokit = new Octokit({
+    auth: process.env.GITHUB_PAT, // Read from environment variable
+});
+
+async function fetchComments() {
+    try {
+        const response = await octokit.request('GET /repos/msupply-foundation/open-msupply/issues/comments', {
+            owner: 'OWNER',
+            repo: 'REPO',
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+            },
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching comments:', error);
+    }
+}
+
+fetchComments();
