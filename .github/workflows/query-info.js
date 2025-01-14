3f59e90029b388ef344f2ebc93b12bbb0f56445a
Fergus Roache <fergus@msupply.foundation>
2025-01-15 10:30:13 +1300

    Update import statement

--- .github/workflows/query-info.js
+++ .github/workflows/query-info.js
@@ -1,4 +1,4 @@
-const { Octokit } = require('@octokit/core');
+import Octokit from '@octokit/core'
 
 const octokit = new Octokit({
     auth: process.env.GITHUB_PAT, // Read from environment variable
