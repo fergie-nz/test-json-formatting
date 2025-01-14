a724266a1cea2be241579d80ad6dae073f870a18
Fergus Roache <fergus@msupply.foundation>
2025-01-15 10:42:20 +1300

    Try calling latest comments

--- .github/workflows/query-info.js
+++ .github/workflows/query-info.js
@@ -6,12 +6,18 @@ const octokit = new Octokit({
 
 async function fetchComments() {
     try {
+        const oneWeekAgo = new Date();
+        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+        // Format the date in ISO 8601 format
+        const since = oneWeekAgo.toISOString();
         const response = await octokit.request('GET /repos/msupply-foundation/open-msupply/issues/comments', {
             owner: 'OWNER',
             repo: 'REPO',
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28',
             },
+            since,
             per_page: 1000,
         });
         console.log(map_to_simplify(response.data));
